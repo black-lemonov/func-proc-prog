1aef06ae83ae490468003cfc219447bb17f3ca37
black-lemonov <egor.porf1@mail.ru>
2024-02-11 12:18:03 +0300

    predicates descriptions fixed, some predicates tested & fixed, nieces_ver2 and grand_mas_ver2 predicates using predicates added

--- lab1/slav_family_tree_w_predicates.pl
+++ lab1/slav_family_tree_w_predicates.pl
@@ -51,134 +51,154 @@ parent(zhdana,zlatomir).
 
 
 
-
+% men/0
 men:- man(X), write(X), nl, fail.
 
+
+% women/0
 women:- woman(Y), write(Y), nl, fail.
 
 
 % +X - unified
 % -X - not unified
-% ?X - ?
+% ?X - unified or not unified
+
 
 % children(+X)
 % DESCRIPTION:
-% writes all the X's children
+% writes X's children
 children(X):- parent(X, Y), write(Y), nl, fail.
 
 
-% mother(+X,-Y)
+% mother(?X, +Y)
 % DESCRIPTION:
 % checks if X is Y's mother
+% or finds Y's mother
 mother(X,Y):- parent(X,Y), woman(X), true.
 
 
 % mother(+X)
 % DESCRIPTION:
-% write the X's mother
-mother(X):- mother(X, Y), write(Y).
+% writes X's mother
+mother(X):- mother(Y, X), write(Y), !.
 
 
-% brother(+X,-Y)
+% brother(?X, +Y)
 % DESCRIPTION:
 % checks if X is Y's brother
-brother(X,Y):- man(X), X \== Y, man(Y), parent(Z, X), parent(Z, Y), true.
+% or finds Y's brother
+brother(X,Y):- man(X), parent(Z, Y), woman(Z), parent(Z, X), X \== Y, true.
 
 
 % brothers(+X)
 % DESCRIPTION:
-% write all the X's brothers
-brothers(X):- brother(X,Y), write(Y), nl, fail.
+% writes X's brothers
+brothers(X):- brother(Y, X), write(Y), nl, fail.
 
 
-% b_s(+X, -Y)
+% b_s(?X, +Y)
 % DESCRIPTION:
-% checks if X and Y are brothers or sisters
-b_s(X, Y):- X \== Y, parent(Z, X), parent(Z, Y), true.
+% checks if X is Y's brother or sister
+% or finds Y's brother or sister
+b_s(X, Y):- parent(Z, X), woman(Z), parent(Z, Y), X \== Y, true.
 
 
 % b_s(+X)
 % DESCRIPTION:
-% writes all the X's brothers and sisters
-b_s(X):- b_s(X,Y), write(Y), nl, fail.
+% writes X's brothers and sisters
+b_s(X):- b_s(Y,X), write(Y), nl, fail.
 
 
-% father(+X, -Y)
+% father(?X, +Y)
 % DESCRIPTION:
 % checks if X is Y's father
+% or finds Y's father
 father(X, Y):- parent(X, Y), man(X), true.
 
 
 % father(+X)
 % DESCRIPTION:
 % writes X's father
-father(X):- father(X,Y), write(Y), !.
+father(X):- father(Y,X), write(Y), !.
 
 
-% wife(+X, -Y)
+% wife(?X, +Y)
 % DESCRIPTION:
 % checks if X is Y's wife
+% or finds Y's wife
 wife(X, Y):- parent(X, Z), parent(Y, Z), woman(X), man(Y), true.
 
 
 % wife(+X)
 % DESCRIPTION:
 % write X's wife
-wife(X):- wife(X, Y), write(Y), !.
+wife(X):- wife(Y, X), write(Y), !.
 
 
-% grand_ma(+X, -Y)
+% grand_ma(?X, +Y)
 % DESCRIPTION:
-% checks if X is Y's grandmother
-% using only facts
+% checks if X is Y's grandmother using facts
+% or finds Y's grandmother
 grand_ma(X, Y):- parent(Z, Y), parent(X, Z), woman(X), true.
 
 
-% grand_ma_ver2(+X, +Y)
+% grand_ma_ver2(?X, +Y)
 % DESCRIPTION:
-% checks if X is Y's grandmother
-% using predicates
-grand_ma_ver2(X, Y):- mother(X, Z), mother(Z, Y), true.
+% checks if X is Y's grandmother using predicate
+% or finds Y's grandmother
+grand_ma_ver2(X, Y):- parent(Z,Y), mother(X,Z), true.
 
 
 % grand_mas(+X)
 % DESCRIPTION:
-% writes all X's grandmothers
-grand_mas(X):- grand_ma(X, Y), write(Y), nl, fail.
+% writes X's grandmothers using facts
+grand_mas(X):- grand_ma(Y, X), write(Y), nl, fail.
+
+
+% grand_mas_ver2(+X)
+% DESCRIPTION:
+% writes X's grandmothers using predicates
+grand_mas_ver2(X):- grand_ma_ver2(Y,X), write(Y), nl, fail.
 
 
 % grand_pa_and_da(+X, +Y)
 % DESCRIPTION:
-% checks if X and Y are grandpa and granddaughter and vice versa
-% using only facts
-grand_pa_and_da(X, Y):- man(X), parent(X, Z), parent(Z, Y), woman(Y); grand_pa_and_da(Y,X), true.
+% checks if X and Y are grandpa and granddaughter and vice versa using facts
+grand_pa_and_da(X, Y):- man(X), parent(X, Z), parent(Z, Y), woman(Y), true.
+grand_pa_and_da(Y, X):- woman(Y), parent(Z, Y), parent(X, Z), man(X), true.
 
 
 % grand_pa_and_da_ver2(+X, +Y)
 % DESCRIPTION:
-% checks if X and Y are grandpa and granddaughter and vice versa
-% using other predicates
-grand_pa_and_da_ver2(X, Y):- wife(Z, X), grand_ma(Z, Y), woman(Y); grand_pa_and_da_ver2(Y, X), true.
+% checks if X and Y are grandpa and granddaughter and vice versa using predicates
+grand_pa_and_da_ver2(X, Y):- wife(Z,X), grand_ma(Z,Y), woman(Y), true.
+grand_pa_and_da_ver2(Y, X):- woman(Y), grand_ma(Z,Y), wife(Z,X), true.
 
 
-% niece(+X, -Y)
+% niece(?X, +Y)
 % DESCRIPTION:
-% checks if X is Y's niece
-% using only facts
-niece(X, Y):- parent(Z,Y), parent(Z,W), Y\==W, parent(W,X), woman(X), true.
+% checks if X is Y's niece using facts
+% or finds Y's niece
+niece(X, Y):- parent(Z,Y), woman(Z), parent(Z,W), Y\==W, parent(W,X), woman(X), true.
 
 
-% niece_ver2(+X, +Y)
+% niece_ver2(?X, +Y)
 % DESCRIPTION:
-% checks if X is Y's niece
-% using other predicates
-niece_ver2(X, Y):- b_s(Y, Z), parent(Z, X), woman(X), true.
+% checks if X is Y's niece using predicates
+% or finds Y's niece
+niece_ver2(X, Y):- b_s(Z, Y), parent(Z, X), woman(X), true.
 
 
-% nieces(+X)
+% nieces(+Y)
 % DESCRIPTION:
-% writes all X's nieces
-nieces(X):- niece(Y,X), write(Y), nl, fail.
+% writes all Y's nieces using facts
+nieces(Y):- niece(X,Y), write(X), nl, fail.
+
+
+% nieces_ver2(+Y)
+% DESCRIPTION:
+% writes all Y's nieces using predicates
+nieces_ver2(Y):- niece_ver2(X,Y), write(X), nl, fail.
 
 
 

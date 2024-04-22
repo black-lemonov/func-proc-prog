29db640db2d0a41606531678d09b0df6be26b289
Егор Парфинцов <egor.porf1@mail.ru>
2024-04-22 23:35:52 +0300

    lab3 task1: add combinations predicate

--- lab3/comb_tasks.pl
+++ lab3/comb_tasks.pl
@@ -1,19 +1,24 @@
 
 % in_list(+List, ?El)
-% checks if El in List
-% or saves List head in El
+% checks if El in List or saves List head in El
 
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
 
 % print_permutations(+Alphabet, +K)
-% prints all permutations of Alphabet el-s
-% with K length
+% prints all permutations of Alphabet el-s with K length
 
 print_permutations(Alphabet, K):- print_permutations(Alphabet, K, []).
 print_permutations(_, 0, Perm):- write(Perm), nl, !, fail.
 print_permutations(Alphabet, K, Perm):- in_list(Alphabet, El), NewK is K - 1, print_permutations(Alphabet, NewK, [El|Perm]). 
 
 
+% print_combinations(+Alphabet, +K)
+% prints all combinations of Alphabet el-s with K length
+
+print_combinations(Alphabet, K):- print_combinations(Alphabet, K, []).
+print_combinations(_, 0, Comb):- write(Comb), nl, !, fail.
+print_combinations([El|Tail], K, Comb):- NewK is K - 1, print_combinations(Tail, NewK, [El|Comb]).
+print_combinations([_|Tail], K, Comb):- print_combinations(Tail, K, Comb).
 

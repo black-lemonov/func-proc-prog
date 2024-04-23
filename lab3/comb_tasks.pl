de93773e9bfe9fb6aab07f2a86583b512d93f508
Егор Парфинцов <egor.porf1@mail.ru>
2024-04-24 02:22:17 +0300

    lab3 task1: add 3 a words predicate and some auxilary predicates

--- lab3/comb_tasks.pl
+++ lab3/comb_tasks.pl
@@ -39,3 +39,26 @@ print_combinations(_, 0, Comb):- write(Comb), nl, !, fail.
 print_combinations([El|Tail], K, Comb):- NewK is K - 1, print_combinations(Tail, NewK, [El|Comb]).
 print_combinations([_|Tail], K, Comb):- print_combinations(Tail, K, Comb).
 
+
+% range(+Start, +End, -Res)
+% saves list with numeric range from Start to End in Res
+
+range(End, Res):- range(0, End, [], Res).
+range(Start, End, Res):- range(Start, End, [], Res).
+range(End, End, CurRes, CurRes):- !.
+range(Start, End, CurRes, Res):- NewStart is Start + 1, range(NewStart, End, [Start|CurRes], Res).  
+
+
+% mix_with_a(+PosComb, +Perm, -Word)
+
+mix_with_a(PosComb, Perm, Word):- mix_with_a(PosComb, Perm, Word, [], 0).
+mix_with_a([], [], CurWord, CurWord, _).
+mix_with_a([H|T], Perm, Word, CurWord, H):- NewI is H + 1, mix_with_a(T, Perm, Word, [a|CurWord], NewI), !.
+mix_with_a(PosComb, [H|T], Word, CurWord, I):- NewI is I + 1, mix_with_a(PosComb, T, Word, [H|CurWord], NewI). 
+
+
+% print_3a_words(+Alphabet, +K)
+
+print_3a_words(Alphabet, K):-
+    range(K, PosList), next_combination(PosList, 3, CurPosComb), NewK is K - 3, next_permutation(Alphabet, NewK, CurPerm),
+    mix_with_a(CurPosComb, CurPerm, CurWord), write(CurWord), nl, fail. 

09653b81174c18011a88a638e5f4afa3af5c5eb2
Егор Парфинцов <egor.porf1@mail.ru>
2024-04-24 17:15:21 +0300

    lab3 task2.2: add predicate to count lines with no spaces

--- lab3/strings_tasks.pl
+++ lab3/strings_tasks.pl
@@ -25,4 +25,60 @@ open_file(FilePath) :-
     write("2.1 Max length = "),
     max_length_of_list(FileStrList, MaxLen),
     write(MaxLen),
+    nl,
+    write("2.2 Lines without spaces = "),
+    count_els_w_no_spaces(FileStrList, CountElsWNoSpaces),
+    write(CountElsWNoSpaces),
     nl.
+
+
+% max_length_ofList(+StrList, +MaxLen)
+% finds max length of list el-s
+
+max_length_of_list(StrList, MaxLen):-
+    max_length_of_list(StrList, 0, MaxLen).
+
+max_length_of_list([], CurMaxLen, CurMaxLen).
+
+max_length_of_list([Line|RestLines], CurMaxLen, MaxLen):-
+    string_length(Line, CurLen),
+    max(CurMaxLen, CurLen, NewCurMaxLen),
+    max_length_of_list(RestLines, NewCurMaxLen, MaxLen).
+
+
+% count_els_w_no_spaces(+strList, +Counter)
+% counts list el-s with no spaces
+
+count_els_w_no_spaces(StrList, Counter):-
+    count_els_w_no_spaces(StrList, 0, Counter).
+
+count_els_w_no_spaces([], CurCounter, CurCounter).
+
+count_els_w_no_spaces([El|RestEls], CurCounter, Counter):- 
+    count(El, ' ', CurElCounter),
+    CurElCounter == 0,
+    NewCurCounter is CurCounter + 1,
+    count_els_w_no_spaces(RestEls, NewCurCounter, Counter),
+    !.
+
+count_els_w_no_spaces([_|RestEls], CurCounter, Counter):-
+    count_els_w_no_spaces(RestEls, CurCounter, Counter).
+
+
+% count(+String, +El, +Count)
+% count El in List
+
+count(String, El, Count) :-
+    string_chars(String, CharList),
+    count(CharList, El, 0, Count).
+
+count([], _, CurCount, CurCount).
+
+count([Char|RestChars], El, CurCount, Count):-
+    Char == El,
+    NewCurCount is CurCount + 1,
+    count(RestChars, El, NewCurCount, Count),
+    !.
+
+count([_|RestChars], El, CurCount, Count):-
+    count(RestChars, El, CurCount, Count).
